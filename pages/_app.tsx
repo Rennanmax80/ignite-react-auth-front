d691993662f5aae104cef60801e610e22e8dfc01
Rennan <rennanmax80@gmail.com>
2021-10-05 22:33:07 -0300

    Auth cookies sessions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { AuthProvider } from '../contexts/AuthContext'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
+
 }
 export default MyApp
